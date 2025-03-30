05ab3ea3065e5c456fe778fbd57302ef14361807
Chisom Maryjane Odo <chisommodo@gmail.com>
2025-03-30 21:12:59 +0100

    hubservice

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
+const BASE_URL = "https://api.github.com/search/users";
+
+export const fetchUsers = async (query, location, minRepos) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    const searchQuery = `q=${query} ${location ? `location:${location}` : ""} ${
+      minRepos ? `repos:>${minRepos}` : ""
+    }`;
+
+    const response = await axios.get(`${BASE_URL}?${searchQuery}`);
+    return response.data.items;
   } catch (error) {
-    throw new Error("User not found");
+    throw new Error("Error fetching GitHub users");
   }
 };
